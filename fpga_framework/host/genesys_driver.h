910994faa35f965058a9a70aa4c59cbc160acd1b
hanyang <hax032@eng.ucsd.edu>
2023-10-28 23:31:49 -0700

    Update host c++ driver code

--- fpga_framework/host/genesys_driver.h
+++ fpga_framework/host/genesys_driver.h
@@ -6,30 +6,8 @@
 #include <CL/cl2.hpp>
 #include <time.h>
 
-/********* Choose Systolic Array Size *********/
-#include <regression_config_SA_16x16.h>
-std::string REPO_PATH = "/home/rohan/serverless_accel_results"
-
-/*
-#ifdef SA_2x2
-    #include <regression_config.h>
-#elif defined(SA_4x4)
-    #include <regression_config_SA_8x8.h>
-#elif defined(SA_8x8)
-    #include <regression_config_SA_8x8.h>
-#elif defined(SA_16x16)
-    #include <regression_config_SA_16x16.h>
-#elif defined(SA32x32)
-    #include <regression_config_SA_8x8.h>
-#elif defined(SA_64x64)
-    #include <regression_config.h>
-#else
-    #include <regression_config.h>
-#endif
-
-*/
-
-/************************************************/
+std::string REPO_PATH = "/home/lavanya/genesys-16x16/tests";
+#include <regression_config_genesys16.h>
 
 // SIZE OF EACH ELEMENT
 #define INT_SIZE 4
@@ -39,107 +17,36 @@ std::string REPO_PATH = "/home/rohan/serverless_accel_results"
 #define BIAS_SIZE 4
 #define OUTPUT_SIZE 4
 
-//#define CUSTOM_CONV_NON_ALIGNED_REQ 1
-    #define NUM_INSTRUCTION 4000 // this has to be in bytes as it is used by hardware. Or change hardware
-    #define NUM_INPUT 13542400
-    #define NUM_WEIGHT 1605632
-    #define NUM_BIAS 128
-    #define NUM_OUTPUT 1605632
-    
-    #define ADDR_OFFSET_INSTRUCTION 0
-    #define ADDR_OFFSET_INPUT 4259840
-    #define ADDR_OFFSET_WEIGHT 24576
-    #define ADDR_OFFSET_BIAS 4096
-    #define ADDR_OFFSET_OUTPUT 0
-    #define ADDR_OFFSET_VMEM1 2097152
-    #define ADDR_OFFSET_VMEM2 4194304
-    #define ADDR_OFFSET_VMEM1_LD 0
-    #define ADDR_OFFSET_VMEM2_LD 262144
-#ifdef RESNET_CONV_64_TILES
-    #define NUM_INSTRUCTION 880 // this has to be in bytes as it is used by hardware. Or change hardware
-    #define NUM_INPUT 13542400
-    #define NUM_WEIGHT 1605632
-    #define NUM_BIAS 128
-    #define NUM_OUTPUT 1605632
+// #define NUM_INSTRUCTION 432 // this has to be in bytes as it is used by hardware. Or change hardware
+// #define NUM_INPUT 2048
+// #define NUM_WEIGHT 2097152
+// #define NUM_BIAS 1024
+// #define NUM_OUTPUT 1024
+#define ADDR_OFFSET_INSTRUCTION 0
 
-    // todo: add a path varialbe and concatenate it to the below file names
-    std::string instruction_file  =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/resnet18_conv_64tiles/resnet18_conv_decimal.txt"; 
-    std::string output_file       =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/resnet18_conv_64tiles/weights_shuffled.txt";
-    std::string input_file        =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/resnet18_conv_64tiles/input_shuffled.txt";
-    std::string weight_file       =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/resnet18_conv_64tiles/weights_shuffled.txt"; 
-    std::string bias_file         =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/resnet18_conv_64tiles/bias.txt"; 
+// #define ADDR_OFFSET_INPUT 26619904
+// #define ADDR_OFFSET_WEIGHT 10403840
+// #define ADDR_OFFSET_BIAS 6160384
+// #define MAX_OFFSET 26619904
+// #define ADDR_OFFSET_OUTPUT 0
+// #define ADDR_OFFSET_VMEM1 0
+// #define ADDR_OFFSET_VMEM2 0
+// #define ADDR_OFFSET_VMEM1_LD 0
+// #define ADDR_OFFSET_VMEM2_LD 0 
 
-    #define ADDR_OFFSET_INSTRUCTION 0
-    #define ADDR_OFFSET_INPUT 4259840
-    #define ADDR_OFFSET_WEIGHT 24576
-    #define ADDR_OFFSET_BIAS 4096
-    #define ADDR_OFFSET_OUTPUT 0
-
-#elif CUSTOM_CONV_NON_ALIGNED_REQ
-    #define NUM_INSTRUCTION 880 // this has to be in bytes as it is used by hardware. Or change hardware
-    #define NUM_INPUT 13542400
-    #define NUM_WEIGHT 1605632
-    #define NUM_BIAS 128
-    #define NUM_OUTPUT 1605632
-
-    // todo: add a path varialbe and concatenate it to the below file names
-    std::string instruction_file  =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/cc_layer_non_aligned_req/cc_layer2_conv_decimal.txt"; 
-    std::string output_file       =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/cc_layer_non_aligned_req/output.txt";
-    std::string input_file        =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/cc_layer_non_aligned_req/input_shuffled.txt";
-    std::string weight_file       =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/cc_layer_non_aligned_req/weights_shuffled.txt"; 
-    std::string bias_file         =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/cc_layer_non_aligned_req/bias.txt"; 
-
-    #define ADDR_OFFSET_INSTRUCTION 0
-    #define ADDR_OFFSET_INPUT 4259840
-    #define ADDR_OFFSET_WEIGHT 24576
-    #define ADDR_OFFSET_BIAS 4096
-    #define ADDR_OFFSET_OUTPUT 0
-/*
-#else
-    // NUMBER OF ELEMENTS
-    #define NUM_INSTRUCTION 880 // this has to be in bytes as it is used by hardware. Or change hardware
-    #define NUM_INPUT 69696
-    #define NUM_WEIGHT 36864
-    #define NUM_BIAS 64
-    #define NUM_OUTPUT 16384
-    // todo: add a path varialbe and concatenate it to the below file names
-    std::string instruction_file  =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/test_custom_conv_conv_partials_cs_v3/custom_conv_conv_decimal.txt"; 
-    std::string output_file       =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/test_custom_conv_conv_partials_cs_v3/output.txt";
-    std::string input_file        =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/test_custom_conv_conv_partials_cs_v3/input_shuffled.txt";
-    std::string weight_file       =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/test_custom_conv_conv_partials_cs_v3/weights_shuffled.txt"; 
-    std::string bias_file         =   "/home/rohan/genesys-merged-repo/genesys-systolic/testbench/test_custom_conv_conv_partials_cs_v3/bias.txt"; 
-    // ADDRESS OFFSETS
-    #define ADDR_OFFSET_INSTRUCTION 0
-    #define ADDR_OFFSET_INPUT 65536
-    #define ADDR_OFFSET_WEIGHT 1048576
-    #define ADDR_OFFSET_BIAS 8192
-    #define ADDR_OFFSET_OUTPUT 0
-*/
-#endif
-
-// TOTAL SIZE OF DATA
-#define INSTRUCTION_SIZE_BYTES (NUM_INSTRUCTION/(INT_SIZE/INSTRUCTION_SIZE)) * INT_SIZE  
-#define INPUT_SIZE_BYTES (NUM_INPUT/(INT_SIZE/INPUT_SIZE)) * INT_SIZE 
-#define WEIGHT_SIZE_BYTES (NUM_WEIGHT/(INT_SIZE/WEIGHT_SIZE)) * INT_SIZE  
-#define BIAS_SIZE_BYTES (NUM_BIAS/(INT_SIZE/BIAS_SIZE)) * INT_SIZE  
-#define OUTPUT_SIZE_BYTES (NUM_OUTPUT/(INT_SIZE/OUTPUT_SIZE)) * INT_SIZE  
-
-
-
-
-#define SCALE_FACTOR 1.1
-#define TOTAL_DATA_SIZE SCALE_FACTOR * (INSTRUCTION_SIZE_BYTES + INPUT_SIZE_BYTES + WEIGHT_SIZE_BYTES + BIAS_SIZE_BYTES)
-//#define TOTAL_DATA_SIZE_INT TOTAL_DATA_SIZE/INT_SIZE 262144 
-#define TOTAL_DATA_SIZE_INT 17902240/INT_SIZE 
-
+#define scaling_factor 1.1
+#define TOTAL_DATA_NUM MAX_OFFSET+(NUM_INSTRUCTION + NUM_INPUT + NUM_WEIGHT + NUM_BIAS)
 #define INSTRUCTION_ADDR_PTR 0
 #define INPUT_ADDR_PTR ADDR_OFFSET_INPUT/INT_SIZE
 #define WEIGHTS_ADDR_PTR ADDR_OFFSET_WEIGHT/INT_SIZE
 #define BIAS_ADDR_PTR ADDR_OFFSET_BIAS/INT_SIZE
 // Use different buffer for output. So initialize it to 0
 //#define OUTPUT_ADDR_PTR OUTPUT_SIZE_BYTES/INT_SIZE
-#define OUTPUT_ADDR_PTR 0
-
+#define OUTPUT_ADDR_PTR ADDR_OFFSET_OUTPUT/INT_SIZE
+#define SIMD_ADDR_VMEM1_PTR ADDR_OFFSET_VMEM1/INT_SIZE
+#define SIMD_ADDR_VMEM1_LD_PTR ADDR_OFFSET_VMEM1_LD/INT_SIZE
+#define SIMD_ADDR_VMEM2_PTR ADDR_OFFSET_VMEM2/INT_SIZE
+#define SIMD_ADDR_VMEM2_LD_PTR ADDR_OFFSET_VMEM2_LD/INT_SIZE
 
 // Helper function declarations
 ;
---
+++ fpga_framework/host/run_regression.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+declare -a tests=( "resnet50_1_conv_case1" "resnet50_1_conv_case101" "resnet50_1_conv_case102" "resnet50_1_conv_case103" "resnet50_1_conv_case104" "resnet50_1_conv_case105" 
+                    "resnet50_1_conv_case106" "resnet50_1_conv_case107" "resnet50_1_conv_case108" "resnet50_1_conv_case109" "resnet50_1_conv_case11" "resnet50_1_conv_case110" 
+                    "resnet50_1_conv_case111" "resnet50_1_conv_case113"  "resnet50_1_conv_case114" "resnet50_1_conv_case115" "resnet50_1_conv_case116" "resnet50_1_conv_case117" 
+                    "resnet50_1_conv_case118" "resnet50_1_conv_case119" "resnet50_1_conv_case120" "resnet50_1_conv_case121" "resnet50_1_conv_case123" "resnet50_1_conv_case124" 
+                    "resnet50_1_conv_case126" "resnet50_1_conv_case127" "resnet50_1_conv_case128" "resnet50_1_conv_case129" "resnet50_1_conv_case13" "resnet50_1_conv_case130"
+                    "resnet50_1_conv_case131" "resnet50_1_conv_case132" "resnet50_1_conv_case133" "resnet50_1_conv_case134" "resnet50_1_conv_case136" "resnet50_1_conv_case15" 
+                    "resnet50_1_conv_case17" "resnet50_1_conv_case18" "resnet50_1_conv_case19" "resnet50_1_conv_case21" "resnet50_1_conv_case22" "resnet50_1_conv_case24"
+                    "resnet50_1_conv_case25" "resnet50_1_conv_case26" "resnet50_1_conv_case27" "resnet50_1_conv_case29" "resnet50_1_conv_case3" "resnet50_1_conv_case30" 
+                    "resnet50_1_conv_case31" "resnet50_1_conv_case32" "resnet50_1_conv_case36" "resnet50_1_conv_case37" "resnet50_1_conv_case38" "resnet50_1_conv_case39"
+                    "resnet50_1_conv_case42" "resnet50_1_conv_case43" "resnet50_1_conv_case44" "resnet50_1_conv_case45" "resnet50_1_conv_case46" "resnet50_1_conv_case47"
+                    "resnet50_1_conv_case48" "resnet50_1_conv_case49" "resnet50_1_conv_case5" "resnet50_1_conv_case50" "resnet50_1_conv_case51" "resnet50_1_conv_case52"
+                    "resnet50_1_conv_case53" "resnet50_1_conv_case54" "resnet50_1_conv_case55" "resnet50_1_conv_case56" "resnet50_1_conv_case57" "resnet50_1_conv_case58"
+                    "resnet50_1_conv_case59" "resnet50_1_conv_case60" "resnet50_1_conv_case61" "resnet50_1_conv_case62" "resnet50_1_conv_case63" "resnet50_1_conv_case64"
+                    "resnet50_1_conv_case66" "resnet50_1_conv_case67" "resnet50_1_conv_case69" "resnet50_1_conv_case7" "resnet50_1_conv_case70" "resnet50_1_conv_case71"
+                    "resnet50_1_conv_case72" "resnet50_1_conv_case73" "resnet50_1_conv_case74" "resnet50_1_conv_case75" "resnet50_1_conv_case76" "resnet50_1_conv_case78"
+                    "resnet50_1_conv_case82" "resnet50_1_conv_case85" "resnet50_1_conv_case88" "resnet50_1_conv_case9" "resnet50_1_conv_case90" "resnet50_1_conv_case94"
+                    "resnet50_1_conv_case95" "resnet50_1_conv_case96" "resnet50_1_conv_case97" "resnet50_1_conv_case98" "resnet50_1_conv_case99"
+)
+ext = "_log.txt"
+fname = ""
+log_name = ""
+for (( i=0; i<1; i++ ));
+do
+  ext = "_log.txt"
+  fname = ${tests[$i]}
+  log_name = "${fname}${ext}"
+  echo "$i Running Test: ${tests[$i]}\n" > log_name
+  #./${tests[$i]} >> log_name
+done
